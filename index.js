bce672f86467ed964c3ec9c14c2f606012d2068f
Elizabeth Vega <eb.vega.m@gmail.com>
2021-02-11 12:50:31 -0300

    funcion de modificar nombre

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const boton = document.getElementById('modificar');
 
 boton.addEventListener('click', () => {
   const nombre = document.getElementById('nombre').value;
-
-  document.getElementById('resultado').innerHTML = nombre;
+  for (let i = 0; i < nombre.length; i++) { 
+    console.log(nombre[i]);
+    document.getElementById('resultado').innerHTML += `<li>${nombre[i]}</li>`;
+  }
+  
 })
